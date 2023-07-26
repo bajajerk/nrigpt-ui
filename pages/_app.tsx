2375fd2f03244239c93f208d066ecb52db3ebc8c
Mayank Bajaj <bajaj3743@gmail.com>
2023-07-26 21:32:50 +0530

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import {  Session } from '@supabase/auth-helpers-react'
 import Layout from '../components/Layout'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({
   Component,
@@ -14,6 +15,7 @@ function MyApp({
     <div className="bg-white">
         <Layout>
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
     </div>
   )

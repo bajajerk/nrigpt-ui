7be28a9c7a4e697c93a53eb8a670c46d2840b5ae
Mayank Bajaj <bajaj3743@gmail.com>
2023-07-26 19:12:09 +0530

    change cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,7 @@ const Home: NextPage = () => {
       }}>
         <div
           className='w-full md:w-1/3  p-6 bg-white border border-gray-200 rounded-lg shadow'>
-          <a href='#'>
-            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Noteworthy technology
-              acquisitions 2021</h5>
-          </a>
+          <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>Noteworthy technology acquisitions 2021</h5>
           <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Here are the biggest enterprise technology
             acquisitions of 2021 so far, in reverse chronological order.</p>
           <a href='#'
@@ -33,10 +30,7 @@ const Home: NextPage = () => {
 
         <div
           className='w-full md:w-1/3 p-6 bg-white border border-gray-200 rounded-lg shadow '>
-          <a href='#'>
-            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Noteworthy technology
-              acquisitions 2021</h5>
-          </a>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>Noteworthy technology acquisitions 2021</h5>
           <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>Here are the biggest enterprise technology
             acquisitions of 2021 so far, in reverse chronological order.</p>
           <a href='#'
